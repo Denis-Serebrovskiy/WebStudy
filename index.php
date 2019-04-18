53a3aaef633c6078b95dc9ce1503b9383accac60
Denis-Serebrovskiy <48431215+Denis-Serebrovskiy@users.noreply.github.com>
2019-04-18 18:30:23 +0300

    Add new file

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+<!DOCTYPE html>
+<html>
+<head>
+	require 'access/data/head.html';
+</head>
+<body>
+	require 'access/data/header.html';
+	require 'access/data/footer.html';
+	require 'access/data/scripts.html';
+</body>
+</html>
+?>
